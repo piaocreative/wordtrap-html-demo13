4aeb4d7b1a1f910b3ab1c2533983112ee5c41350
piaocreative <johnpiao171019@gmail.com>
2021-09-07 22:47:39 +0800

    demo13-0907

--- js/main.js
+++ js/main.js
@@ -73,6 +73,38 @@ $( document ).ready( function() {
         ]
     });
 
+    $('.logos').slick({
+      dots: false,
+      arrows: false,
+      speed: 300,
+      slidesToShow: 9,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            dots: false
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+  });
+
 } );
 
 
